150643e7a84ade575862f39347f50eeb4634802c
daido1976 <daido1976@gmail.com>
2021-08-25 23:46:39 +0900

    Show type definition

--- bmi.hs
+++ bmi.hs
@@ -1,7 +1,9 @@
 import System.Environment (getArgs)
 
+main :: IO ()
 main = do
   (heightString : weightString : _) <- getArgs
   print (bmi (read heightString) (read weightString))
 
+bmi :: Fractional a => a -> a -> a
 bmi height weight = weight / height ^ 2
--- fizzbuzz.hs
+++ fizzbuzz.hs
@@ -1,15 +1,18 @@
 import Control.Monad
 
+main :: IO ()
 main = do
   putStr "Input number: "
   numStr <- getLine
   let num = read numStr
   fizzBuzz num
 
+fizzBuzz :: (Show a, Integral a) => a -> IO ()
 fizzBuzz num = do
   forM_ [1 .. num] $ \i -> do
     putStrLn (show i ++ ": " ++ toFizzBuzz i)
 
+toFizzBuzz :: (Integral a, Show a) => a -> [Char]
 toFizzBuzz num =
   case mod num 15 of
     0 -> "FizzBuzz"
