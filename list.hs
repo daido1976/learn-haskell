dee1c6df2ca5b09ee9aba6076b5454f1315b17ae
daido1976 <daido1976@gmail.com>
2021-08-29 12:15:44 +0900

    Refacto quickSort

--- list.hs
+++ list.hs
@@ -15,4 +15,7 @@ isAsc (x : y : xs) = (x <= y) && isAsc (y : xs)
 
 quickSort :: (Ord a) => [a] -> [a]
 quickSort [] = []
-quickSort (x : xs) = quickSort [y | y <- xs, y <= x] ++ [x] ++ quickSort [y | y <- xs, y > x]
+quickSort (x : xs) =
+  let smallerOrEqual = [a | a <- xs, a <= x]
+      bigger = [a | a <- xs, a > x]
+   in quickSort smallerOrEqual ++ [x] ++ quickSort bigger
